6f1df24033dc1b10fad4a2085ffeb36f924f3944
Romanna Yasinska <r.bren.1991@gmail.com>
2022-04-26 00:05:02 +0300

    upd api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 const KEY = '25289922-76fc98c8dd80f80668ef47aa3';
 export const fetchImages = async (name, page = 1, limit = 20) => {
-  try {
   const params = {
     key: KEY,
     q: name,
@@ -11,8 +10,5 @@ export const fetchImages = async (name, page = 1, limit = 20) => {
     };
     const response = await axios.get(`https://pixabay.com/api/`, { params });
     return response.data;  
-  }  catch (error) {
-   return `Picture ${name} didn't find`;
-   }
 };
 
